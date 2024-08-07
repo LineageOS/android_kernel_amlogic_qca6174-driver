2b6374f79ef0f311cb65c10cd9e8e7b1672df74e
jiabin.chen <jiabin.chen@amlogic.com>
2024-08-07 16:27:32 +0800

    wifi: fix build errors after kernel upgrade [1/1]
    PD#SWPL-180577
    
    Problem:
    kernel upgrade
    
    Solution:
    fix build errors
    
    Verify:
    build
    
    Change-Id: Ie985f140b3cfd0422225947e555560ba5529b1ca
    Signed-off-by: jiabin.chen <jiabin.chen@amlogic.com>

--- AIO/drivers/qcacld-new/CORE/HDD/src/wlan_hdd_hostapd.c
+++ AIO/drivers/qcacld-new/CORE/HDD/src/wlan_hdd_hostapd.c
@@ -1501,24 +1501,34 @@ hdd_update_chandef(hdd_adapter_t *hostapd_adapter,
 #endif
 
 #ifdef CFG80211_SINGLE_NETDEV_MULTI_LINK_SUPPORT
-static inline
-void wlan_cfg80211_ch_switch_notify(struct net_device *dev,
-				    struct cfg80211_chan_def *chandef,
-				    unsigned int link_id)
+static inline void wlan_cfg80211_ch_switch_notify(struct net_device *dev,
+	struct cfg80211_chan_def *chandef, unsigned int link_id)
 {
-#if ((defined (AML_KERNEL_VERSION) && AML_KERNEL_VERSION >= 15) || LINUX_VERSION_CODE >= KERNEL_VERSION(5, 15, 153))
-	cfg80211_ch_switch_notify(dev, chandef, link_id, 0);
+#if ( (defined (CONFIG_AMLOGIC_KERNEL_VERSION) && defined (AML_KERNEL_VERSION)) && (\
+		(CONFIG_AMLOGIC_KERNEL_VERSION == 13515 && AML_KERNEL_VERSION >= 15)\
+	 || (CONFIG_AMLOGIC_KERNEL_VERSION == 14515 && AML_KERNEL_VERSION >= 12) ) )\
+	 || (LINUX_VERSION_CODE >= KERNEL_VERSION(6, 3, 0) && LINUX_VERSION_CODE < KERNEL_VERSION(6, 9, 0))
+	 return cfg80211_ch_switch_notify(dev, &chandef, link_id, 0);
+#elif defined (CFG80211_SINGLE_NETDEV_MULTI_LINK_SUPPORT)
+	 return cfg80211_ch_switch_notify(dev, &chandef, link_id);
 #else
-	cfg80211_ch_switch_notify(dev, chandef, link_id);
+	 return cfg80211_ch_switch_notify(dev, &chandef);
 #endif
 }
-#else
-static inline
-void wlan_cfg80211_ch_switch_notify(struct net_device *dev,
-				    struct cfg80211_chan_def *chandef,
-				    unsigned int link_id)
+
+static inline void wlan_cfg80211_ch_switch_started_notify(struct net_device *dev,
+	struct cfg80211_chan_def *chandef, unsigned int link_id, u8 count, bool quiet)
 {
-	cfg80211_ch_switch_notify(dev, chandef);
+#if ( (defined (CONFIG_AMLOGIC_KERNEL_VERSION) && defined (AML_KERNEL_VERSION)) && (\
+		(CONFIG_AMLOGIC_KERNEL_VERSION == 13515 && AML_KERNEL_VERSION >= 15)\
+	 || (CONFIG_AMLOGIC_KERNEL_VERSION == 14515 && AML_KERNEL_VERSION >= 12) ) )\
+	 || (LINUX_VERSION_CODE >= KERNEL_VERSION(6, 3, 0) && LINUX_VERSION_CODE < KERNEL_VERSION(6, 9, 0))
+	 return cfg80211_ch_switch_started_notify(dev, &chandef, link_id, count, quiet, 0);
+#elif defined (CFG80211_SINGLE_NETDEV_MULTI_LINK_SUPPORT)
+	 return cfg80211_ch_switch_started_notify(dev, &chandef, link_id, count, quiet);
+#else
+	 return cfg80211_ch_switch_started_notify(dev, &chandef, count);
+#endif
 }
 #endif
 
